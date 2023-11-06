a3ab351a5b5bf1be3ec99a5bbc8d69bd5ee542e3
Administrator <@>
2023-11-06 18:49:34 +0900

    temp save

--- app/src/main/java/com/example/myapplication/ex008_ktor/KtorViewModel.kt
+++ app/src/main/java/com/example/myapplication/ex008_ktor/KtorViewModel.kt
@@ -3,7 +3,7 @@ package com.example.myapplication.ex008_ktor
 import androidx.lifecycle.ViewModel
 import com.example.myapplication.ex009_weather.Weather
 import com.example.myapplication.ex009_weather.WeatherReq
-import com.example.myapplication.ex009_weather.WeatherResponse
+import com.example.myapplication.ex009_weather.WeatherRes
 import com.google.gson.Gson
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
@@ -20,25 +20,29 @@ import java.io.InputStreamReader
 class KtorViewModel: ViewModel() {
     fun req() {
         CoroutineScope(Dispatchers.IO).launch {
+
             // param set
-            val api = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst"
             val weatherReq = WeatherReq(
                 baseDate = "20231106",
                 baseTime = "1800",
                 nx = 55,
                 ny = 127
             )
+            val api = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst"
             val url = "$api${weatherReq.getQueryString()}"
 
             // request
             val httpClient = HttpClient(CIO)
             val httpResponse: HttpResponse = httpClient.get(url)
             val inputStreamReader = InputStreamReader(ByteArrayInputStream(httpResponse.readBytes()))
-            httpClient.close()
 
             // data transform
-            val weatherResponse: WeatherResponse = Gson().fromJson(inputStreamReader, WeatherResponse::class.java)
-            val weather: Weather = weatherResponse.response.body.toWeather()
+            val weatherRes: WeatherRes = Gson().fromJson(inputStreamReader, WeatherRes::class.java)
+            val weather: Weather = weatherRes.response.body.toWeather()
+
+            // release
+            inputStreamReader.close()
+            httpClient.close()
 
             // output
             println("------------------------------------------------------------")
--- app/src/main/java/com/example/myapplication/ex009_weather/WeatherRes.kt
+++ app/src/main/java/com/example/myapplication/ex009_weather/WeatherRes.kt
@@ -1,6 +1,6 @@
 package com.example.myapplication.ex009_weather
 
-data class WeatherResponse(
+data class WeatherRes(
     val response: Response,
 )
 
@@ -44,12 +44,12 @@ data class Body(
             when (it.category) {
                 "PTY" -> { pty = it.obsrValue } // 강수형태 / 코드값 / (초단기) 없음(0), 비(1), 비/눈(2), 눈(3), 빗방울(5), 빗방울눈날림(6), 눈날림(7)
                 "REH" -> { reh = it.obsrValue } // 습도 / %
-                "RN1" -> { rn1 = it.obsrValue }  // 1시간 강수량 / mm
-                "T1H" -> { t1h = it.obsrValue }  // 기온 / ℃
-                "UUU" -> { uuu = it.obsrValue }  // 동서바람성분 / m/s
-                "VEC" -> { vec = it.obsrValue }  // 풍향 / deg
-                "VVV" -> { vvv = it.obsrValue }  // 남북바람성분 / m/s
-                "WSD" -> { wsd = it.obsrValue }  // 풍속 / m/s
+                "RN1" -> { rn1 = it.obsrValue } // 1시간 강수량 / mm
+                "T1H" -> { t1h = it.obsrValue } // 기온 / ℃
+                "UUU" -> { uuu = it.obsrValue } // 동서바람성분 / m/s
+                "VEC" -> { vec = it.obsrValue } // 풍향 / deg
+                "VVV" -> { vvv = it.obsrValue } // 남북바람성분 / m/s
+                "WSD" -> { wsd = it.obsrValue } // 풍속 / m/s
             }
         }
 
